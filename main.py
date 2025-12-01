6d248088f8ac984399c5a2ae3a6d264845cb1e18
Aya Sghaier <aya.sghaier@layxo.com>
2025-12-01 13:53:35 +0100

    Description des modifications

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("hello world  !")
-print("This is a simple Python script.")
+seq= []
+
+print ("Enter the number of elements in the sequence:")
+n = int(input())
+print ("Enter the elements of the sequence:")
+
+for i in range(0, n):
+    element = int(input())
+    seq.append(element)
+
+for i in range (n-1) :
+    if seq[i] == seq[i+1] :
+        print("true")
+        break
+    else :
+        print("false")
+        break
+
